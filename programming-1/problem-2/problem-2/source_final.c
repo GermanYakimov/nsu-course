b4c6e1fe21420b54ff8aaa041e35111926dcc8c2
German Yakimov <german13yakimov@gmail.com>
2020-03-15 16:53:26 +0700

    Generalize switch for symbol (construction) matching.

--- programming-1/problem-2/problem-2/source_final.c
+++ programming-1/problem-2/problem-2/source_final.c
@@ -8,7 +8,14 @@
 #define PATTERN_SIZE 64
 #define WORD_SIZE 1024
 
+typedef struct match_result {
+	char *pattern_reader;
+	char *word_reader;
+	int match;
+} state;
+
 char *compare_iterations(char *word, char *pattern);
+state match(char* pattern, char* word);
 
 char **read_words(FILE* file, int number, char **words)
 {
@@ -126,11 +133,12 @@ char *compare_symbol(char symbol, char *pattern)
 	return pattern;
 }
 
-
 char *compare_kleene_star(char *word, char *pattern)
 {
 	char *word_reader = word, *pattern_reader = strstr(pattern, "<") + 1;
 	char *pattern_end = strstr(pattern, ">"), *pattern_start = pattern_reader, *last_position = word_reader;
+
+	state match_result;
 
 	for (int i = 0; i < INT_MAX; i++)
 	{
@@ -138,31 +146,41 @@ char *compare_kleene_star(char *word, char *pattern)
 
 		while (*word_reader && pattern_reader != pattern_end)
 		{
-			switch (*pattern_reader)
+			match_result = match(pattern_reader, word_reader);
+
+			if (!match_result.match)
 			{
-			case '[':
-				word_reader = compare_iterations(word_reader, pattern_reader);
-
-				if (!word_reader)
-				{
-					return last_position;
-				}
-
-				pattern_reader = strstr(pattern_reader, "]") + 1;
-				break;
-
-			case '\\':
-			case '~':
-			default:
-				pattern_reader = compare_symbol(*word_reader, pattern_reader);
-
-				if (!pattern_reader)
-				{
-					return last_position;
-				}
-
-				word_reader++;
+				return last_position;
 			}
+
+			pattern_reader = match_result.pattern_reader;
+			word_reader = match_result.word_reader;
+
+			//switch (*pattern_reader)
+			//{
+			//case '[':
+			//	word_reader = compare_iterations(word_reader, pattern_reader);
+
+			//	if (!word_reader)
+			//	{
+			//		return last_position;
+			//	}
+
+			//	pattern_reader = strstr(pattern_reader, "]") + 1;
+			//	break;
+
+			//case '\\':
+			//case '~':
+			//default:
+			//	pattern_reader = compare_symbol(*word_reader, pattern_reader);
+
+			//	if (!pattern_reader)
+			//	{
+			//		return last_position;
+			//	}
+
+			//	word_reader++;
+			//}
 		}
 
 		if (!(*word_reader))
@@ -182,36 +200,53 @@ char *compare_iterations(char *word, char *pattern)
 	char *word_reader = word, *pattern_reader = strstr(pattern, "(") + 1;
 	char *pattern_end = strstr(pattern, ")"), *pattern_start = pattern_reader;
 
+	state match_result;
+
 	for (int i = 0; i < iterations; i++)
 	{
 		pattern_reader = pattern_start;
 
 		while (*word_reader && pattern_reader != pattern_end)
 		{
-			switch (*pattern_reader)
-			{
-			case '<':
-				word_reader = compare_kleene_star(word_reader, pattern_reader);
-				pattern_reader = strstr(pattern_reader, ">") + 2;
-				break;
+			match_result = match(pattern_reader, word_reader);
 
-			case '\\':
-			case '~':
-			default:
-				pattern_reader = compare_symbol(*word_reader, pattern_reader);
-
-				if (!pattern_reader)
-				{
-					return NULL;
-				}
-
-				word_reader++;
-			}
-
-			if (!(*word_reader))
+			if (!match_result.match)
 			{
 				return NULL;
 			}
+
+			pattern_reader = match_result.pattern_reader;
+			word_reader = match_result.word_reader;
+
+			//switch (*pattern_reader)
+			//{
+			//case '<':
+			//	word_reader = compare_kleene_star(word_reader, pattern_reader);
+			//	pattern_reader = strstr(pattern_reader, ">") + 2;
+			//	break;
+
+			//case '\\':
+			//case '~':
+			//default:
+			//	pattern_reader = compare_symbol(*word_reader, pattern_reader);
+
+			//	if (!pattern_reader)
+			//	{
+			//		return NULL;
+			//	}
+
+			//	word_reader++;
+			//}
+
+			//if (!(*word_reader))
+			//{
+			//	return NULL;
+			//}
+		}
+
+		if (!(*word_reader))
+		{
+			return NULL;
 		}
 	}
 
@@ -221,42 +256,108 @@ char *compare_iterations(char *word, char *pattern)
 int compare(char *word, char *pattern)
 {
 	char* word_reader = word, *pattern_reader = pattern;
+	state match_result;
 
 	while (*word_reader && *pattern_reader)
 	{
-		switch (*pattern_reader)
+		match_result = match(pattern_reader, word_reader);
+
+		if (!match_result.match)
 		{
-		case '[':
-			word_reader = compare_iterations(word_reader, pattern_reader);
-
-			if (!word_reader)
-			{
-				return 0;
-			}
-
-			pattern_reader = strstr(pattern_reader, "]") + 1;
-			break;
-
-		case '<':
-			word_reader = compare_kleene_star(word_reader, pattern_reader);
-			pattern_reader = strstr(pattern_reader, ">") + 2;
-			break;
-
-		case '\\':
-		case '~':
-		default:
-			pattern_reader = compare_symbol(*word_reader, pattern_reader);
-
-			if (!pattern_reader)
-			{
-				return 0;
-			}
-
-			word_reader++;
+			return 0;
 		}
+
+		word_reader = match_result.word_reader;
+		pattern_reader = match_result.pattern_reader;
+
+		//switch (*pattern_reader)
+		//{
+		//case '[':
+		//	word_reader = compare_iterations(word_reader, pattern_reader);
+
+		//	if (!word_reader)
+		//	{
+		//		return 0;
+		//	}
+
+		//	pattern_reader = strstr(pattern_reader, "]") + 1;
+		//	break;
+
+		//case '<':
+		//	word_reader = compare_kleene_star(word_reader, pattern_reader);
+		//	pattern_reader = strstr(pattern_reader, ">") + 2;
+		//	break;
+
+		//case '\\':
+		//case '~':
+		//default:
+		//	pattern_reader = compare_symbol(*word_reader, pattern_reader);
+
+		//	if (!pattern_reader)
+		//	{
+		//		return 0;
+		//	}
+
+		//	word_reader++;
+		//}
 	}
 
 	return 1;
+}
+
+state match(char *pattern, char *word)
+{
+	char *pattern_reader = pattern, *word_reader = word;
+
+	state result;
+	result.match = 0;
+	result.pattern_reader = pattern_reader;
+	result.word_reader = word_reader;
+
+	switch (*pattern_reader)
+	{
+	case '[':
+		word_reader = compare_iterations(word_reader, pattern_reader);
+
+		if (!word_reader)
+		{
+			result.match = 0;
+			result.pattern_reader = pattern_reader;
+			result.word_reader = word_reader;
+
+			return result;
+		}
+
+		pattern_reader = strstr(pattern_reader, "]") + 1;
+		break;
+
+	case '<':
+		word_reader = compare_kleene_star(word_reader, pattern_reader);
+		pattern_reader = strstr(pattern_reader, ">") + 2;
+		break;
+
+	case '\\':
+	case '~':
+	default:
+		pattern_reader = compare_symbol(*word_reader, pattern_reader);
+
+		if (!pattern_reader)
+		{
+			result.match = 0;
+			result.pattern_reader = pattern_reader;
+			result.word_reader = word_reader;
+
+			return result;
+		}
+
+		word_reader++;
+	}
+
+	result.match = 1;
+	result.pattern_reader = pattern_reader;
+	result.word_reader = word_reader;
+
+	return result;
 }
 
 int main()
@@ -264,31 +365,31 @@ int main()
 	char pattern[PATTERN_SIZE];
 	FILE *input, *output;
 	input = fopen("input.txt", "r");
-	
+
 	if (!input)
 	{
 		printf("Can't open input.txt");
 		return -1;
 	}
-	
+
 	int number, matches_count = 0;
 	fscanf(input, "%s", pattern);
 	fscanf(input, "%d", &number);
-	
+
 	char** words = allocate_words_array(number);
-	
+
 	if (!words)
 	{
 		printf("Can't allocate memory for words array");
 		return -1;
 	}
-	
+
 	words = read_words(input, number, words);
 
 	fclose(input);
 
 	output = fopen("output.txt", "w");
-	
+
 	if (!output)
 	{
 		printf("Can't open output.txt");
@@ -303,13 +404,13 @@ int main()
 			matches_count++;
 		}
 	}
-	
+
 	if (!matches_count)
 	{
 		fprintf(output, "none");
 	}
 	fclose(output);
-	
+
 	free_words_array(words, number);
 
 	return 0;
