175a3bc0738622398a19e6d87901592b825261a0
German Yakimov <german13yakimov@gmail.com>
2020-10-31 16:30:52 +0700

    Implement basic task (pass tests on nsuts).

--- programming-2/problem-3/problem-3/main.cpp
+++ programming-2/problem-3/problem-3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <fstream>
 using namespace std;
 
@@ -22,6 +23,7 @@ template <typename K, typename V>
 class List
 {
 	Node<K, V>* head;
+	size_t elements;
 
 	Node<K, V>* get_pointer(K key)
 	{
@@ -46,9 +48,14 @@ class List
 	}
 
 public:
-	List() : head(nullptr) {}
+	List() : head(nullptr), elements(0) {}
 
-	List(K key, V value): head(new Node<K, V>(key, value, nullptr)) {}
+	List(K key, V value): head(new Node<K, V>(key, value, nullptr)), elements(1) {}
+
+	size_t size()
+	{
+		return this->elements;
+	}
 
 	V get(K key)
 	{
@@ -66,6 +73,20 @@ public:
 		throw invalid_argument("Node with given key doesn't exist.");
 	}
 
+	set<V> unique_elements()
+	{
+		set<V> result;
+		Node<K, V>* tmp = this->head;
+
+		while (tmp)
+		{
+			result.insert(tmp->value);
+			tmp = tmp->next;
+		}
+
+		return result;
+	}
+
 	void rewrite_or_add(K key, V value)
 	{
 		Node<K, V>* element = this->get_pointer(key);
@@ -73,6 +94,7 @@ public:
 		if (!element)
 		{
 			this->append(key, value);
+			this->elements++;
 		}
 		else
 		{
@@ -98,6 +120,7 @@ public:
 					this->head = current->next;
 				}
 
+				this->elements--;
 				delete current;
 				return;
 			}
@@ -106,7 +129,7 @@ public:
 			current = current->next;
 		}
 
-		throw invalid_argument("Node with given key doesn't exist.");
+		//throw invalid_argument("Node with given key doesn't exist.");
 	}
 
 	~List()
@@ -132,29 +155,42 @@ class HashMap
 
 	static const size_t default_size = 10000;
 	static const size_t portion = 5000;
-	size_t current_size, filled;
 
 	size_t get_index(K key)
 	{
-		return this->hasher(key) % current_size;
+		return this->hasher(key) % this->data.size();
 	}
 
 	bool need_to_resize()
 	{
-		return filled / current_size > 0.7;
+		return this->size() / this->data.size() > 0.7;
 	}
 
 	void resize()
 	{
-		this->data.resize(this->current_size + portion);
-		this->current_size = this->current_size + portion;
+		this->data.resize(this->data.size() + portion);
 	}
 
 public:
-	HashMap(): current_size(default_size), filled(0)
+	HashMap()
 	{
 		data.resize(default_size);
 		fill(data.begin(), data.end(), nullptr);
+	}
+
+	size_t size()
+	{
+		size_t result = 0;
+
+		for (size_t i = 0; i < this->data.size(); i++)
+		{
+			if (this->data[i])
+			{
+				result += this->data[i]->size();
+			}
+		}
+
+		return result;
 	}
 
 	V get(K key)
@@ -175,14 +211,28 @@ public:
 	{
 		size_t index = this->get_index(key);
 
-		if (!this->data[index])
-		{
-			throw invalid_argument("Element with given key doesn't exist.");
-		}
-		else
+		if (this->data[index])
 		{
 			this->data[index]->remove(key);
 		}
+	}
+
+	size_t unique_elements()
+	{
+		size_t result = 0;
+		set<V> elements;
+		set<V> elements_current;
+
+		for (size_t i = 0; i < this->data.size(); i++)
+		{
+			if (this->data[i])
+			{
+				elements_current = this->data[i]->unique_elements();
+				elements.insert(elements_current.begin(), elements_current.end());
+			}
+		}
+
+		return elements.size();
 	}
 
 	void add(K key, V value)
@@ -192,7 +242,6 @@ public:
 		if (!this->data[index])
 		{
 			this->data[index] = new List<K, V>(key, value);
-			filled++;
 
 			if (this->need_to_resize())
 			{
@@ -207,7 +256,7 @@ public:
 
 	~HashMap()
 	{
-		for (size_t i = 0; i < this->current_size; i++)
+		for (size_t i = 0; i < this->data.size(); i++)
 		{
 			if (this->data[i])
 			{
@@ -217,40 +266,101 @@ public:
 	}
 };
 
-
-template <typename K>
-K read_key(istream& input)
+template <typename K, typename V>
+void do_commands(istream& input, size_t commands_number)
 {
+	HashMap<K, V> dict;
+	K key;
+	V value;
+	char command;
 
-}
+	for (size_t i = 0; i < commands_number; i++)
+	{
+		input >> command;
 
-template <typename V>
-V read_value(istream& input)
-{
+		switch (command)
+		{
+		case 'R':
+			input >> key;
+			dict.remove(key);
+			break;
+		case 'A':
+			input >> key;
+			input >> value;
+			dict.add(key, value);
+			break;
+		default:
+			break;
+		}
+	}
 
+	cout << dict.size() << " " << dict.unique_elements() << endl;
 }
 
 int main()
 {
 	ifstream input("input.txt");
 	char k_type, v_type;
+	size_t commands_number;
 	input >> k_type >> v_type;
-	cout << k_type << " " << v_type;
+	input >> commands_number;
+	//cout << k_type << " " << v_type;
 
 	switch (k_type)
 	{
 	case 'I':
-		int key;
+		switch (v_type)
+		{
+		case 'I':
+			do_commands<int, int>(input, commands_number);
+			break;
+		case 'D':
+			do_commands<int, double>(input, commands_number);
+			break;
+		case 'S':
+			do_commands<int, string>(input, commands_number);
+			break;
+		default:
+			throw invalid_argument("Incorrect value type.");
+		}
 		break;
 	case 'D':
-		double key;
+		switch (v_type)
+		{
+		case 'I':
+			do_commands<double, int>(input, commands_number);
+			break;
+		case 'D':
+			do_commands<double, double>(input, commands_number);
+			break;
+		case 'S':
+			do_commands<double, string>(input, commands_number);
+			break;
+		default:
+			throw invalid_argument("Incorrect value type.");
+		}
 		break;
 	case 'S':
-		string key;
+		switch (v_type)
+		{
+		case 'I':
+			do_commands<string, int>(input, commands_number);
+			break;
+		case 'D':
+			do_commands<string, double>(input, commands_number);
+			break;
+		case 'S':
+			do_commands<string, string>(input, commands_number);
+			break;
+		default:
+			throw invalid_argument("Incorrect value type.");
+		}
 		break;
 	default:
 		throw invalid_argument("Incorrect key type.");
 	}
+
+	input.close();
 
 	return EXIT_SUCCESS;
 }
